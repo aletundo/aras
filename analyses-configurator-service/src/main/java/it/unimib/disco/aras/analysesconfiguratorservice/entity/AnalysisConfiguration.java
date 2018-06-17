4fc89efb0be7a050ddf4b1e33cbb21038c5aa4f4
Alessandro Tundo <alessandrotundo94@gmail.com>
2018-06-17 18:29:19 +0200

    [analyses-configurator-service] Updated to SpringBoot 2
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- analyses-configurator-service/src/main/java/it/unimib/disco/aras/analysesconfiguratorservice/entity/AnalysisConfiguration.java
+++ analyses-configurator-service/src/main/java/it/unimib/disco/aras/analysesconfiguratorservice/entity/AnalysisConfiguration.java
@@ -1,33 +1,19 @@
 package it.unimib.disco.aras.analysesconfiguratorservice.entity;
 
-import java.util.Date;
 import java.util.Map;
-import java.util.UUID;
 
-import javax.persistence.Convert;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import org.springframework.data.annotation.Id;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
 @NoArgsConstructor
-@Entity
 public class AnalysisConfiguration {
     
     @Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
-    private UUID id;
-    
-    private UUID projectId;
-    
-    private UUID versionId;
-    
-    private Date startTime;
-    
-    @Convert(converter = JsonConverter.class)
-    private Map<String, String> params;
+    private String id;
+    private String projectId;
+    private String versionId;
+    private Map<String, String> arcanParameters;
 }
