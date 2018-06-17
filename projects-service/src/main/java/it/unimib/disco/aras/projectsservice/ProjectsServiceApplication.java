aadca0b5d00d1ea94bfecdc4ee62b7ee85f4a396
Alessandro Tundo <alessandrotundo94@gmail.com>
2018-06-17 18:02:09 +0200

    [projects-service] Updated to SpringBoot 2
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- projects-service/src/main/java/it/unimib/disco/aras/projectsservice/ProjectsServiceApplication.java
+++ projects-service/src/main/java/it/unimib/disco/aras/projectsservice/ProjectsServiceApplication.java
@@ -2,10 +2,8 @@ package it.unimib.disco.aras.projectsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ProjectsServiceApplication {
 
 	public static void main(String[] args) {
---
+++ projects-service/src/main/java/it/unimib/disco/aras/projectsservice/config/DiscoveryClientConfiguration.java
@@ -0,0 +1,10 @@
+package it.unimib.disco.aras.projectsservice.config;
+
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableDiscoveryClient
+public class DiscoveryClientConfiguration {
+
+}
