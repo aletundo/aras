dc644b0019ed37ec1e92a31601dd0cb1008612bf
Alessandro Tundo <alessandrotundo94@gmail.com>
2018-06-17 17:44:19 +0200

    [reports-service] Updated to SpringBoot 2 and other minor fixes
    Signed-off-by: Alessandro Tundo <alessandrotundo94@gmail.com>

--- reports-service/src/main/java/it/unimib/disco/aras/reportsservice/ReportsServiceApplication.java
+++ reports-service/src/main/java/it/unimib/disco/aras/reportsservice/ReportsServiceApplication.java
@@ -2,10 +2,8 @@ package it.unimib.disco.aras.reportsservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
 public class ReportsServiceApplication {
 
 	public static void main(String[] args) {
